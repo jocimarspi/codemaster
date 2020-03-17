a5447c5109f2b1283733fe0168b26148cbc5a4da
Jocimar B. A. Huss <jocimarspi@gmail.com>
2020-03-17 13:06:29 -0300

    Squire challange 5 - Palindrome

---
+++ Delphi/5_palindromo/palindromo.dpr
@@ -0,0 +1,21 @@
+program palindromo;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  uConsole in 'uConsole.pas';
+
+var
+  Console: TConsole;
+
+begin
+  Console := TConsole.Create;
+  try
+    Console.Run;
+  finally
+    FreeAndNil(Console);
+  end;
+end.
---
+++ Delphi/5_palindromo/uConsole.pas
@@ -0,0 +1,91 @@
+﻿unit uConsole;
+
+interface
+
+uses
+  System.Classes,
+  System.SysUtils,
+  System.StrUtils;
+
+type
+  TConsole = class
+    FPhrasaList: TStringList;
+    FHasPhrase: Boolean;
+  private
+    function HasPhrase: Boolean;
+    procedure AddPhrase;
+    procedure ShowPalindromos;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure Run;
+  end;
+
+implementation
+
+{ TConsole }
+
+constructor TConsole.Create;
+begin
+  FPhrasaList := TStringList.Create;
+end;
+
+destructor TConsole.Destroy;
+begin
+  FreeAndNil(FPhrasaList);
+  inherited;
+end;
+
+function TConsole.HasPhrase: Boolean;
+begin
+  Result := FHasPhrase;
+end;
+
+procedure TConsole.Run;
+begin
+  FPhrasaList.Clear;
+  try
+    Writeln('Informe uma frase e pressione ENTER para incluí-la.');
+    Writeln('Pressione ENTER duas vezes para mostrar somente os palíndromos.');
+    Writeln('');
+
+    repeat
+      AddPhrase;
+    until not HasPhrase;
+
+    ShowPalindromos;
+
+    Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end;
+
+procedure TConsole.ShowPalindromos;
+var
+  Phrase: String;
+begin
+  Writeln('');
+  Writeln('Palíndromos encontrados');
+  for Phrase in FPhrasaList do
+  begin
+    if Phrase.LowerCase(Phrase.Trim)= Phrase.LowerCase(ReverseString(Phrase.Trim)) then
+      Writeln(Format(' - %s', [Phrase]));
+  end;
+end;
+
+procedure TConsole.AddPhrase;
+var
+  Phrase: String;
+begin
+  Write(Format('Phrase(%d): ',[FPhrasaList.Count+1]));
+  Readln(Phrase);
+
+  FHasPhrase := not Phrase.Trim.IsEmpty;
+
+  if FHasPhrase then  
+    FPhrasaList.Add(Phrase.Trim);
+end;
+
+end.
